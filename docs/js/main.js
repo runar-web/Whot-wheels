05af18f364f8fa3dc06e7b35345e42b30ff12125
Runar <github.runar@gmail.com>
2020-12-31 13:45:06 +0300

    fixed bugs

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,43 +1,39 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  //Nav menu
+  document.querySelector(".menu-icon-wrapper").onclick = function () {
+    document.querySelector(".menu-icon").classList.toggle("menu-icon-active");
+  };
 
-    //Nav menu
-    document.querySelector('.menu-icon-wrapper').onclick = function() {
-        document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
-    }
-
-    // Owl-carousel
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-    });
-
-    // Scroll To Top
-    const csrollBtn = $('.scroll-to-top-btn');
-
-    // По скроллу скрываем или показываем кнопку
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 500 ) {
-            csrollBtn.fadeIn();
-        } else {
-            csrollBtn.fadeOut();
-        }
-    });
-
-    // Клик по кнопке 
-    csrollBtn.click(function(){
-     $('html, body').animate({scrollTop: 0 }, 360);
-     return false;
-    });
-
-    // MixItup
-    var mixer = mixitup('.products__row');
-
- 
-  //slide2id - плавная прокрутка по ссылкам внутри страницы
-  $("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
-    highlightSelector: "nav a"
-
+  // Owl-carousel
+  $(".owl-carousel").owlCarousel({
+    items: 1,
   });
 
+  // Scroll To Top
+  const csrollBtn = $(".scroll-to-top-btn");
 
+  // По скроллу скрываем или показываем кнопку
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+      csrollBtn.fadeIn();
+    } else {
+      csrollBtn.fadeOut();
+    }
+  });
 
+  // Клик по кнопке
+  csrollBtn.click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 360);
+    return false;
+  });
+
+  // MixItup
+  var mixer = mixitup(".products__row");
+
+  //slide2id - плавная прокрутка по ссылкам внутри страницы
+  $(
+    "nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id"
+  ).mPageScroll2id({
+    highlightSelector: "nav a",
+  });
 });
